1a8bb0254e8be6174ba99a8da3d1ece50675fe8c
Wilsonhouston30 <hou22004@byui.edu>
2024-01-10 12:29:13 -0700

    W01 Prove: Developer—C# Programming Parts 1–2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Xml.Schema;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        var userGrade = Console.ReadLine();
+        var plusMinus = userGrade[1];
+        int userNum = int.Parse(userGrade);
+        var letterGrade = "";
+        Console.WriteLine(plusMinus);
+
+
+        if (userNum >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (userNum >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (userNum >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (userNum >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your grade is a: {letterGrade}");
+
+        if (userNum >= 70)
+        {
+            System.Console.WriteLine("You are passing the class!");
+        }
+        else
+        {
+            System.Console.WriteLine("Keep on working, you will get better! ");
+        }
+
     }
 }
